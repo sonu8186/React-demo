bc59f848d2bb537c9e02b8aaf51b38f6e7fc4bae
thakur-sonu123 <thakurharrysingh123@gmail.com>
2025-10-02 11:44:00 +0530

    name change button added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { useState } from "react";
+
 const App=()=>{
+  const [name,setname]= useState("sonu singh");
+  const handle=()=>{
+    setname("Amit kumar")
+  }
   return(
+   
     <>
-    <h1>sonu singh</h1>
+    <h1>name :{name}</h1>
+    <p>i am web developer</p>
+    <button  onClick={handle}  className="btn" >Clcik Me</button>
     </>
   )
 }
